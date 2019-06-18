3e97f892511e6133e303792b0cf2bb43ddfe8321
leoacosta <hello@iamleoacosta.com>
2019-06-18 22:18:11 +1000

    Initial commit: dabbling in hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [loanAmount, setLoanAmount] = useState(0);
+    const [interestRate, setInterestRate] = useState(6.99);
+
+    function getLoanAmount(event) {
+        console.log(event.target.value);
+        setLoanAmount(event.target.value);
+    }
+
+    function getInterestRate(event) {
+        console.log(event.target.value);
+        setInterestRate(event.target.value);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Estimat</p>
+                <h1>Estimat</h1>
+                <div className="mb-1">
+                    <label className="mb-1">Loan Amount</label>
+                    <input
+                        type="text"
+                        value={loanAmount}
+                        onChange={getLoanAmount}
+                    />
+                </div>
+                <div className="mb-1">
+                    <label className="mb-1">Interest Rate</label>
+                    <input
+                        type="text"
+                        value={interestRate}
+                        onChange={getInterestRate}
+                    />
+                </div>
+                <div>
+                    <h2>$0</h2>
+                </div>
             </header>
         </div>
     );
